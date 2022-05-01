09ff0bd4d1a25afa3290cc13ee647577bf5b98e3
Atalay <atalaydurdu@gmail.com>
2022-05-01 19:27:28 -0400

    added new file sample

--- sample.py
+++ sample.py
@@ -1,3 +1,9 @@
 # sample file for git
 
 print(" Hello world ")
+print(" more lines in the sample1 ")
+for i in range(10)
+    print(f"numbers : {i} ")
+
+
+print(" New changes after last push, fixed PROJ-23455")
